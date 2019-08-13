2ee3151a3cdc8271bf932a51bc2136c829115db8
Leopoldo C <leopoldo.cuellar.m94@gmail.com>
2019-08-13 00:05:13 -0500

    Updates

---
+++ precache-manifest.fdd6805c1bb4cc36b79928c5780839c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a96a3160e48dab69e19bddb0019ccd1",
+    "url": "/cryptoapp/index.html"
+  },
+  {
+    "revision": "532c6c4d135f07ef2cbd",
+    "url": "/cryptoapp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "41ade8a8e6da65a03226",
+    "url": "/cryptoapp/static/js/2.892ea8d4.chunk.js"
+  },
+  {
+    "revision": "532c6c4d135f07ef2cbd",
+    "url": "/cryptoapp/static/js/main.e1c4ef02.chunk.js"
+  },
+  {
+    "revision": "9428c1e0e6399e2862e6",
+    "url": "/cryptoapp/static/js/runtime~main.c3c8c278.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cryptoapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
